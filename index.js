0e4f26251036aff92000d52a4a5607b6e1fb8bea
tkamanda <toelokemwa@gmail.com>
2024-07-22 10:48:23 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// remove the main element with id main
+document.querySelector('main#main').remove();
+
+//create a new h1 element
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "Toel is the champion";
+
+document.body.appendChild(newHeader);
